f5386b7d7a88a8c8b1fb60e70e6fc20dff27f5a7
ewyy-design <ewyy.design@gmail.com>
2022-04-01 00:14:57 +0800

    Finish tempConversion exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+  temp = (num - 32) * 5/9;
+  if (temp % 1 == 0) {
+    return temp;
+  } else {
+    return Number(temp.toFixed(1));
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+  temp = (num * 9/5) + 32;
+  if (temp % 1 == 0) {
+    return temp;
+  } else {
+    return Number(temp.toFixed(1));
+  }
 };
 
 // Do not edit below this line
