b2ac141afbd72d9a6a2707e0195f38a52a333fca
ewyy-design <ewyy.design@gmail.com>
2022-03-31 23:49:43 +0800

    4/6 completed sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(first, ...last) {
+    let sum = 0;
 
-};
+    if (first > last) {
+        for (i=first;i>=last;i--) {
+            sum = sum + i
+        }
+        return sum
+    } else if (first < 0 || last < 0 && typeof last == 'string') {
+        return ('ERROR');
+    } else {
+        for (i=first;i<=last;i++) {
+            sum = sum + i;
+        }
+        return sum;
+    }    
+}
+    
+sumAll(10, "90")
 
 // Do not edit below this line
 module.exports = sumAll;
