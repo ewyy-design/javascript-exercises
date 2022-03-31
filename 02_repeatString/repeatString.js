a4b4b1371f9762f64873637e4e05304cd1825590
ewyy-design <ewyy.design@gmail.com>
2022-03-31 13:19:35 +0800

    Finish repeatString exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const randomNumber = function() {
+    const number = Math.floor(Math.random() * 1000) 
+    return number;
+}
 
+const repeatString = function(string, num) {
+    
+    let output = "";
+    if (num<0) {
+        return ('ERROR')
+    } else {
+        for (let i=0;i<num;i++) {
+            output = output + string;
+        }
+        return output; 
+    } 
 };
+
+repeatString('', 10);
 
 // Do not edit below this line
 module.exports = repeatString;
