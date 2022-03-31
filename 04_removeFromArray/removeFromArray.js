239c529f76b8b1ed3ec149fa7780f650e4fa2d2a
ewyy-design <ewyy.design@gmail.com>
2022-03-31 22:40:29 +0800

    4/7 completed removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...toRemove) {
+    for (i=0;i<array.length;i++) {
+        if (array.includes(toRemove[i])) {
+            array.splice(array.indexOf(toRemove[i]), 1)
+        }
+    }
+    return array;
 };
+
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4)
 
 // Do not edit below this line
 module.exports = removeFromArray;
